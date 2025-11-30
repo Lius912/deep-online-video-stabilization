edf1c1f1d7df25892b6bd0aab434b6fa0cbc145e
Lev Lysyk <levlisik@gmail.com>
2025-11-30 20:05:16 +0200

    process communication fix

--- masa_immitator.py
+++ masa_immitator.py
@@ -8,8 +8,24 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--test-list', nargs='+')
-parser.add_argument('--production-dir', nargs='+')
+parser.add_argument('--model-dir')
+parser.add_argument('--model-name')
+parser.add_argument('--before-ch', type=int)
+#parser.add_argument('--after-ch', type=int)
+parser.add_argument('--output-dir', default='data_video_local')
+parser.add_argument('--infer-with-stable', action='store_true')
+parser.add_argument('--infer-with-last', action='store_true')
+parser.add_argument('--test-list', nargs='+', default=['data_video/test_list', 'data_video/train_list_deploy'])
+#parser.add_argument('--train-list', default='data_video/train_list_deploy')
+parser.add_argument('--prefix', default='data_video')
+parser.add_argument('--max-span', type=int, default=1)
+parser.add_argument('--random-black', type=int, default=None)
+#parser.add_argument('--indices', type=int, nargs='+', required=True)
+parser.add_argument('--start-with-stable', action='store_true')
+parser.add_argument('--refine', type=int, default=1)
+parser.add_argument('--no_bm', type=int, default=1)
+parser.add_argument('--gpu_memory_fraction', type=float, default=0.1)
+parser.add_argument('--deploy-vis', action='store_true')
 args = parser.parse_args()
 
 video_list = []
