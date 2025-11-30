b04d750ab258fbdefe8bf7a7ce139aa44ddbc6aa
Lev Lysyk <levlisik@gmail.com>
2025-11-30 17:49:04 +0200

    trying true width height

--- deploy_bundle_my.py
+++ deploy_bundle_my.py
@@ -336,6 +336,8 @@ for video_name in video_list:
     unstable_cap = cv2.VideoCapture(os.path.join(args.prefix,'unstable', video_name))
     
     fps = unstable_cap.get(cv2.CAP_PROP_FPS)
+    width = cv2.CAP_PROP_FRAME_WIDTH
+    height = cv2.CAP_PROP_FRAME_HEIGHT
     
 
     print(fps)
