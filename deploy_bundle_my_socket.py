e07f3d7c64c9f496bc76e32bee8a170ffa69018e
Lev Lysyk <levlisik@gmail.com>
2025-12-11 04:17:50 +0200

    fixing gt remapping

--- deploy_bundle_my_socket.py
+++ deploy_bundle_my_socket.py
@@ -203,7 +203,10 @@ def get_bbox(mask):
 
 def remap(x1, y1, x2, y2, x_map, y_map):
     # TODO hcange to zeroes carefullly
-    mask = np.ones((output_width, output_height))
+    mask = np.ones((output_width, output_height)) * 255
+    mask = mask.astype(np.uint8)
+    
+    mask = cv2.resize(mask, (output_width, output_height))
     
     cv2.rectangle(mask, (int(x1), int(y1)), (int(x2), int(y2)), 0, -1)
     # display_resized(mask, width, height)
